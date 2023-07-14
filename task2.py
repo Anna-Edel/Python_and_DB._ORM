bd7e4fc621719ea19b8248e83dc39d2c21d26b76
Anna-Edel <125572175+Anna-Edel@users.noreply.github.com>
2023-07-14 13:33:10 +0200

    Update task2.py
    correction2

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Publisher, Book, Sale, Shop
+from models import Publisher, Book, Sale, Shop, Stock
 
 # Установите соединение с базой данных
 engine = create_engine('postgresql://postgres:"введите свой пароль Postgres"@localhost:5432/"Введите название вашей БД"')
@@ -11,9 +11,10 @@ session = Session()
 publisher_name = input("Введите имя или идентификатор издателя: ")
 
 # Выполните запрос выборки
-sales = session.query(Book.title, Shop.name, Sale.price, Sale.date)\
-    .join(Sale, Book.id == Sale.book_id)\
-    .join(Shop, Sale.shop_id == Shop.id)\
+sales = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)\
+    .join(Stock, Book.id == Stock.book_id)\
+    .join(Sale, Stock.id == Sale.stock_id)\
+    .join(Shop, Stock.shop_id == Shop.id)\
     .join(Publisher, Book.publisher_id == Publisher.id)\
     .filter(Publisher.name == publisher_name)\
     .all()
